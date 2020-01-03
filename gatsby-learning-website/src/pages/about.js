b25de5113a66780b9850f641feb1783f6999961e
Michael Su <xmichael.su@gmail.com>
2020-01-02 22:48:43 -0800

    create a about me page

---
+++ gatsby-learning-website/src/pages/about.js
@@ -0,0 +1,15 @@
+import Reach from "react"
+
+const aboutPage = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        This is the about me page! This currently a placeholder! More will
+        follow!
+      </p>
+    </div>
+  )
+}
+
+export default aboutPage
