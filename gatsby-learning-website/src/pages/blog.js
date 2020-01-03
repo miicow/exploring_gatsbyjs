c96d152f2342f4e4d5c1bb9413d76f774da98f5b
Michael Su <xmichael.su@gmail.com>
2020-01-02 22:42:56 -0800

    update variable name, add blog page. site routes

---
+++ gatsby-learning-website/src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const blogPage = () => {
+  return (
+    <div>
+      <h1>My Blog</h1>
+      <p>This is currently a place holder.</p>
+    </div>
+  )
+}
+
+export default blogPage
--- gatsby-learning-website/src/pages/index.js
+++ gatsby-learning-website/src/pages/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 
-const Index = () => {
+const indexPage = () => {
   return (
     <div>
       <h1>Hello</h1>
-      <h2>I'm Michael, a Software Engineer living in beautiful Bay Area</h2>
+      <h2>I'm Michael, a Software Engineer living in beautiful Bay Area!</h2>
     </div>
   )
 }
 
-export default Index
+export default indexPage
