d89833536db21632a973e0f941cc2bb6abaa3198
Michael Su <xmichael.su@gmail.com>
2020-01-02 22:50:41 -0800

    add a contact me page

---
+++ gatsby-learning-website/src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const contactPage = () => {
+  return (
+    <div>
+      <h1>Contact me</h1>
+      <li>
+        <ul>Email</ul>
+        <ul>LinkedIn</ul>
+        <ul>Github</ul>
+      </li>
+    </div>
+  )
+}
+
+export default contactPage
