9c51687dd29c9760a6c5493a0ca5caf429254d75
Michael Su <xmichael.su@gmail.com>
2020-01-03 23:13:57 -0800

    implement link between with anchor tag, rerenders

--- gatsby-learning-website/src/pages/index.js
+++ gatsby-learning-website/src/pages/index.js
@@ -5,6 +5,10 @@ const indexPage = () => {
     <div>
       <h1>Hello</h1>
       <h2>I'm Michael, a Software Engineer living in beautiful Bay Area!</h2>
+
+      <p>
+        Need a developer? <a href="/contact">Contact me.</a>
+      </p>
     </div>
   )
 }
