570da4689cb0557aa5756b664eb7517e8b969100
Michael Su <xmichael.su@gmail.com>
2020-01-03 23:26:34 -0800

    implement Link to contact page from about page

--- gatsby-learning-website/src/pages/about.js
+++ gatsby-learning-website/src/pages/about.js
@@ -1,13 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const aboutPage = () => {
   return (
     <div>
       <h1>About Page</h1>
-      <p>
-        This is the about me page! This currently a placeholder! More will
-        follow!
-      </p>
+      <p>This is the about me page! Work in progress!</p>
+      <Link to="/contact">Contact Me!</Link>
     </div>
   )
 }
