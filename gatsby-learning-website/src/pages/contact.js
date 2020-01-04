cc3f26de621b5cc5e1985ecb2df71b5cda4c74ab
Michael Su <xmichael.su@gmail.com>
2020-01-03 23:24:27 -0800

    include working links on contact page

--- gatsby-learning-website/src/pages/contact.js
+++ gatsby-learning-website/src/pages/contact.js
@@ -3,11 +3,19 @@ import React from "react"
 const contactPage = () => {
   return (
     <div>
-      <h1>Contact me</h1>
+      <h1>Contact Me!</h1>
       <ul>
-        <li>Email</li>
-        <li>LinkedIn</li>
-        <li>Github</li>
+        <li>
+          <a href="mailto:michaelsu.jobs@gmail.com">Email</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/michael-su-407a10157/">
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/miicow">GitHub</a>
+        </li>
       </ul>
     </div>
   )
