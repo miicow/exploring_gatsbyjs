398e49f6984ddb4d43c7247b7fef225e1f26254a
Michael Su <xmichael.su@gmail.com>
2020-01-03 23:21:02 -0800

    remove anchor tag link

--- gatsby-learning-website/src/pages/index.js
+++ gatsby-learning-website/src/pages/index.js
@@ -8,10 +8,6 @@ const indexPage = () => {
     <div>
       <h1>Hello</h1>
       <h2>I'm Michael, a Software Engineer living in beautiful Bay Area!</h2>
-
-      <p>
-        Need a developer? <a href="/contact">Contact me.</a>
-      </p>
       <p>
         Need a developer? <Link to="/contact">Contact Me.</Link>
       </p>
