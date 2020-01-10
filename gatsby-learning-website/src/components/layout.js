7539c7f66ba8a07a4c054ac478425991c16e78f1
Michael Su <xmichael.su@gmail.com>
2020-01-10 00:13:59 -0800

    universal shared layout component for site pages

---
+++ gatsby-learning-website/src/components/layout.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const Layout = () => {
+  return <div></div>
+}
+
+export default Layout
+//this will eventually become the only thing needed to be imported for our pages
