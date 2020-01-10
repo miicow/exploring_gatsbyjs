3aa9f17ecad16f7f089b3b1da0075095d8eb9aa1
Michael Su <xmichael.su@gmail.com>
2020-01-09 23:44:14 -0800

    update header with Link - navigate through pages

--- gatsby-learning-website/src/components/header.js
+++ gatsby-learning-website/src/components/header.js
@@ -1,7 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Header = () => {
-  return <div>Home - About - Blog - Contact</div>
+  return (
+    <div>
+      <Link to="">Home</Link>
+      <Link to="about">About</Link>
+      <Link to="blog">Blog</Link>
+      <Link to="contact">Contact</Link>
+    </div>
+  )
 }
 
 export default Header
