cdf5a5ea154b4329df7b3c0c1651c12b536f1872
Michael Su <xmichael.su@gmail.com>
2020-01-13 09:39:21 -0800

    implement styling for header component

--- gatsby-learning-website/src/components/header.js
+++ gatsby-learning-website/src/components/header.js
@@ -11,26 +11,53 @@ import headerStyles from "./header.module.scss"
 
 const Header = () => {
   return (
-    <header>
-      <h1>Michael Su</h1>
+    <header className={headerStyles.header}>
+      <h1>
+        <Link className={headerStyles.title} to="/">
+          Michael Su
+        </Link>
+      </h1>
 
       <nav>
-        <ul>
+        <ul className={headerStyles.navList}>
           <li>
-            <Link to="/" className={headerStyles.link}>
+            {/* using activeClassName allows your web page to display visual cue for users */}
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/"
+            >
               Home
             </Link>
           </li>
 
           <li>
-            <Link to="/about">About</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/about"
+            >
+              About
+            </Link>
           </li>
 
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/blog"
+            >
+              Blog
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/contact"
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
